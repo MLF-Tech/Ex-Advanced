66a0a46520577aa66daf4e5e9a301ead12a14468
Maria-Laura <m.laurafagioli@gmail.com>
2025-03-24 23:24:40 +0100

    all changes staged

--- ts/script.js
+++ ts/script.js
@@ -1,3 +1,4 @@
 "use strict";
+// Define for each variable/array the data type
 let menu = ["Appetizers", "Main Course", "Second Course", "Desserts", "Beverages", "Coffee"];
 console.log(menu);
--- ts/script.ts
+++ ts/script.ts
@@ -1,3 +1,5 @@
+// Define for each variable/array the data type
+
 let menu: string [] = ["Appetizers", "Main Course", "Second Course", "Desserts", "Beverages", "Coffee"];
 
 console.log(menu)
