7c63bd46d043f3e571bb850e1a36977930b2e6da
Maria-Laura <m.laurafagioli@gmail.com>
2025-03-24 23:39:23 +0100

    Display data with loops

--- ts/script.js
+++ ts/script.js
@@ -1,4 +1,13 @@
 "use strict";
-// Define for each variable/array the data type
+// Define for each variable/array the data type with a strict data type definition
 let menu = ["Appetizers", "Main Course", "Second Course", "Desserts", "Beverages", "Coffee"];
+//list the array items
 console.log(menu);
+//Displaying data with loops
+//forEach
+menu.forEach(function (value, index) {
+    console.log(`This is our menu ${value} with index ${index}`);
+});
+//Do we want also trying the other loops? Do you want to try one?
+//for in
+//for.of
