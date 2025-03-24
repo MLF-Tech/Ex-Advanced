fc6fe842ee64e87a01a5c0377ef95353c482a65e
Maria-Laura <m.laurafagioli@gmail.com>
2025-03-24 20:08:31 +0100

    array creation

---
+++ ts/script.ts
@@ -0,0 +1,32 @@
+let menu : {title: string; description: string; price: number;} []= [
+{
+    title: "Appetizers",
+    description: "Delight in a fusion of Viennese charm and Italian flavors with our handcrafted antipasti, featuring fresh bruschetta, creamy burrata, and delicate carpaccio.",
+    price: 9.99,    
+},
+{
+    title: "Main course",
+    description: "Savor the heart of Italy with our exquisite main courses, from rich homemade pasta and perfectly wood-fired pizzas to tender osso buco and fresh seafood, all crafted with the finest ingredients.",
+    price: 20,    
+},
+{
+    title: "Second course",
+    description: "Indulge in our expertly prepared second courses, featuring succulent grilled meats, delicate seafood specialties, and classic Italian dishes, served with authentic flavors and a touch of Viennese elegance.",
+    price: 30,    
+},
+{
+    title: "Desserts",
+    description: "End on a sweet note with our irresistible homemade tiramisu, creamy panna cotta, and delicate pastries, bringing the taste of Italy to Vienna.",
+    price: 15,    
+},
+{
+    title: "Beverages",
+    description: "Complement your meal with our selection of fine Italian wines, refreshing aperitifs, handcrafted cocktails, and premium non-alcoholic drinks.",
+    price: 7.99,    
+},
+{
+    title: "Coffee",
+    description: "Experience true Italian coffee culture with our rich espresso, velvety cappuccino, and perfectly brewed specialties, made from the finest beans.",
+    price: 2.5,    
+},
+]
