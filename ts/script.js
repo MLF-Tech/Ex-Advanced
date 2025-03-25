f92285c65c95f016373278c2843312f32a024139
Julia-S08 <julia.schallmeiner@aon.at>
2025-03-25 22:34:47 +0100

    adding the for in and for of loop; and a favicon

--- ts/script.js
+++ ts/script.js
@@ -1,8 +1,11 @@
 "use strict";
 // Define for each variable/array the data type with a strict data type definition
-let menu = ["Appetizers", "Main Course", "Second Course", "Desserts", "Beverages", "Coffee"];
+// COMM JULIA: i agree with you that we should declare the menu as a const! I did already change it at the top. hope that's fine for you. I like the subcategories. But not sure if we need them. what will we do with them? let's talk about it tomorrow! :)
+
+const menu = ["Appetizers", "Main Course", "Second Course", "Desserts", "Beverages", "Coffee"];
 //list the array items
 console.log(menu);
+
 //Displaying data with loops
 //forEach
 menu.forEach(function (value, index) {
@@ -10,7 +13,19 @@ menu.forEach(function (value, index) {
 });
 //Do we want also trying the other loops? Do you want to try one?
 //for in
+// COMM JUlIA: did the for in Loop here; but don't know if it really makes sense to use it as this loop is actually for objects;
+let txt1 = "";
+for (const x in menu) {
+    txt1 += menu[x];
+}
+console.log(txt1);
 //for.of
+// COMM JULIA: did the for of loop here. 
+let txt2 = "";
+for (const men of menu) {
+    txt2 += men + " ";
+}
+console.log(txt2);
 // Define some appropriate value as const:
 // At this point I would suggest to call menu as const and then create variables (let) for each course, like this:
 // Definition of menu as Const
@@ -25,6 +40,4 @@ menu.forEach(function (value, index) {
 //We could also use a mixed array for the let ones to show the prices, for example:
 //let appetizers:Array<string | number> = ["Bruschetta", 10, "Tagesuppe", 5, "Tagliere di formaggi", 15]
 
-$('.carousel').carousel({
-    interval: 2000
-  })
+
--- ts/script.ts
+++ ts/script.ts
@@ -1,5 +1,5 @@
 // Define for each variable/array the data type with a strict data type definition
-let menu: string [] = ["Appetizers", "Main Course", "Second Course", "Desserts", "Beverages", "Coffee"];
+const menu: string [] = ["Appetizers", "Main Course", "Second Course", "Desserts", "Beverages", "Coffee"];
 
 //list the array items
 console.log(menu)
@@ -12,9 +12,30 @@ menu.forEach(function(value, index){
 
 //Do we want also trying the other loops? Do you want to try one?
 //for in
+// COMM JUlIA: did the for in Loop here; but don't know if it really makes sense to use it as this loop is actually for objects;
+
+
+let txt1: string ="";
+
+for (const x in menu){
+    txt1 += menu[x];
+}
+
+console.log(txt1);
 
 
 //for.of
+// COMM JULIA: did the for of loop here. :)
+
+let txt2: string = "";
+
+for (const men of menu) {
+   txt2 +=  men + " ";
+}
+
+console.log(txt2);
+
+
 
 
 // Define some appropriate value as const:
@@ -34,3 +55,7 @@ menu.forEach(function(value, index){
 //We could also use a mixed array for the let ones to show the prices, for example:
 //let appetizers:Array<string | number> = ["Bruschetta", 10, "Tagesuppe", 5, "Tagliere di formaggi", 15]
 
+
+
+
+// COMM JULIA: i agree with you that we should declare the menu as a const! I did already change it at the top. hope that's fine for you. I like the subcategories. But not sure if we need them. what will we do with them? let's talk about it tomorrow! :)
